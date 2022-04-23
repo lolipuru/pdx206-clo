0c9d36d51b1576af1e042ef4c44740b119719767
1nfrag <sil3ntcll@gmail.com>
2022-04-23 11:54:52 +0800

    pdx206: Switch to AOSP NFC stack
    * Align to AOSP NFC template config.
    
    Depends-On: I691d0654579ad137958904284d8d9022ca49c924
    Change-Id: Ia288b2666b8eed5664fbcfcccda511ac9e067c20

--- device.mk
+++ device.mk
@@ -181,7 +181,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
 
 PRODUCT_COPY_FILES += \
