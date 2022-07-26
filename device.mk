4c6b3c1dc94d11b9693c551ff4af9b30f3507b56
Luofan Chen <clfbbn@gmail.com>
2022-07-26 18:35:22 +0800

    pdx206: Import sony charger control HAL
    Signed-off-by: Luofan Chen <clfbbn@gmail.com>

--- device.mk
+++ device.mk
@@ -28,7 +28,8 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/sony
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
