635fff89f22a6df78137b5312a19243c2f124a63
Luofan Chen <clfbbn@gmail.com>
2022-08-10 22:20:03 +0800

    pdx206: Import puiblic.libraries.txt
    Signed-off-by: Luofan Chen <clfbbn@gmail.com>

--- device.mk
+++ device.mk
@@ -22,6 +22,10 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_API_LEVEL)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Additional native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
