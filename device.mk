27e4b098b44f48f2f33c67195c0b45e38931f01c
Jake Weinstein <jake@aospa.co>
2022-12-09 00:35:52 +0900

    pdx206: Do not balance msm_drm and kgsl_3d0 IRQs
    These are critical for display performance and should not be balanced
    in order to improve latency and responsiveness.
    
    And also affine them.
    
    Change-Id: If49ecb8757d133a7fad0d7946837b35403e57c2a

--- device.mk
+++ device.mk
@@ -221,6 +221,10 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
