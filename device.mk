23a9db4dfba420a4f5a72f531e46f43e2ca2897d
kyasu <kyasu.dev@gmail.com>
2023-01-22 10:22:46 +0900

    pdx206: nfc: Add config file for FeliCa (Type-F).
    * Set NXP_SYS_CLK_SRC_SEL to CLK_SRC_XTAL
    * This fixes 'libnfc_nci: unknown opcode:0x21' error on XQ-AS42.

--- device.mk
+++ device.mk
@@ -181,6 +181,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp-typef.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp-typef.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
 
 PRODUCT_COPY_FILES += \
