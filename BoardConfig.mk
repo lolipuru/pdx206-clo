f23dd5d1f533f36ab94a278adb81f5c55aaff7a2
dantmnf <dantmnf2@gmail.com>
2023-02-15 23:07:24 +0800

    pdx206: Enable adaptive refresh rate
    Use conservative idle timer to reduce flickering
    
    Change-Id: Ibdacf6f04a790d955a8d44912926191194f9168c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,6 +120,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/properties/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/properties/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
 
