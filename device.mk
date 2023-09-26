4249ec581f62decea6c7037c47c31ac91f3cc2d4
lolipuru <banstellazz@gmail.com>
2023-09-27 02:33:12 +0700

    pdx206: import lights hal & rebrand for edo
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- device.mk
+++ device.mk
@@ -128,6 +128,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.sony_edo
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
